c39725ad44b597e8e87bc4bf0fd457513a8ba38b
arpitpatawat <75129076+arpitpatawat@users.noreply.github.com>
2022-05-21 15:46:45 +0530

    Time: 2 ms (31.52%), Space: 44.3 MB (47.31%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -13,8 +13,8 @@ class Solution {
                 sum = 0;
             }
         }
-        if(sum !=0 )return fsum + sum;
-        return fsum;
+        return fsum + sum;
+        // return fsum;
         
     }
 }
