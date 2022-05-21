ebf76d7ee3f07de586d617bfa3b0b5918c6ce989
arpitpatawat <75129076+arpitpatawat@users.noreply.github.com>
2022-05-21 14:41:29 +0530

    Time: 3 ms (52.24%), Space: 84.3 MB (21.69%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -4,7 +4,8 @@ class Solution {
         int sum = 0;
         for(int i =1 ; i < prices.length ; i++ ){
             if(prices[i] > min){
-            sum = Math.max(sum , prices[i] - min);
+            sum = sum>prices[i] - min ? sum: prices[i]-min;
+                //sum = Math.max(sum , prices[i] - min);
             }
             else min = prices[i];
         }
