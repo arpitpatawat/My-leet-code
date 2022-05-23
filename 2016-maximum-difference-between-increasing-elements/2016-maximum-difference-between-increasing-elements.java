15093785182c96a884bd1df0c54c4d6a06aa2a46
arpitpatawat <75129076+arpitpatawat@users.noreply.github.com>
2022-05-23 20:06:08 +0530

    Time: 42 ms (5.43%), Space: 44 MB (7.36%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.java
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.java
@@ -1,23 +1,25 @@
+// class Solution {
+//     public int maximumDifference(int[] nums) {
+//       int mn = nums[0], res = -1;
+//     for (int i = 1; i < nums.length; ++i) {
+//         res = Math.max(res, nums[i] - mn);
+//         mn = Math.min(mn, nums[i]);
+//     }
+//     return res == 0 ? -1 : res; 
+//     }
+// } 
+
 class Solution {
-    public int maximumDifference(int[] nums) {
-      int mn = nums[0], res = -1;
-    for (int i = 1; i < nums.length; ++i) {
-        res = Math.max(res, nums[i] - mn);
-        mn = Math.min(mn, nums[i]);
-    }
-    return res == 0 ? -1 : res; 
+    public int maximumDifference(int[] a) {
+        int res = a[0], diff = -1;
+        for(int i =1 ; i < a.length ; i++){
+            System.out.println(diff);
+                diff = Math.max(diff, a[i] - res);
+                res = Math.min(res,a[i]);
+        }
+        return diff == 0 ? -1:diff;
     }
 } 
 
 
 
-
-// int res = 0, diff = -1;
-//         for(int i =1 ; i < a.length ; i++){
-//             if(a[i] > a[res]){
-//                 diff = Math.max(diff, a[i] - a[res]);
-//             }
-//             else 
-//                 res = i;
-//         }
-//         return diff;
