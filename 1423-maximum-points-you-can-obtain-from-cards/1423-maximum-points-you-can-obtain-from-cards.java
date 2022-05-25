25b69e5bebfd36e1c3f8b31aeba1ac64ef56be85
arpitpatawat <75129076+arpitpatawat@users.noreply.github.com>
2022-05-26 00:13:40 +0530

    Time: 4 ms (28.23%), Space: 64.7 MB (71.35%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,17 +1,31 @@
 class Solution {
     public int maxScore(int[] a, int k) {
-      int t = a.length - k;
-        int arrsum = 0;
-    for(int i = 0 ; i < t ; ++i){
-        arrsum += a[i];
-    }
-        int currsum = arrsum;
-        int minsum = currsum;
-    for(int i = t; i <= a.length -1; ++i){
-        currsum += a[i] - a[i-t];
-        minsum = Math.min(minsum,currsum);
-        arrsum += a[i];
-    }
-        return arrsum - minsum;
+    //   int t = a.length - k;
+    //     int arrsum = 0;
+    // for(int i = 0 ; i < t ; ++i){
+    //     arrsum += a[i];
+    // }
+    //     int currsum = arrsum;
+    //     int minsum = currsum;
+    // for(int i = t; i <= a.length -1; ++i){
+    //     currsum += a[i] - a[i-t];
+    //     minsum = Math.min(minsum,currsum);
+    //     arrsum += a[i];
+    // }
+    //     return arrsum - minsum;
+        
+        
+        int n=a.length, res=0;
+        
+        for(int i=0;i<k;i++) res+=a[i];
+        int curr_sum=res;
+        int j=1;
+        for(int i=k-1;i>=0;i--) {
+            // cout << curr_sum << endl;
+            curr_sum = curr_sum-a[i]+a[n-j];
+            j++;
+            res=Math.max(res, curr_sum);
+        }
+        return res;
     }
 }
