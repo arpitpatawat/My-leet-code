5bf879b7dd00c5d15645e37816bc4af5be471734
arpitpatawat <75129076+arpitpatawat@users.noreply.github.com>
2022-05-26 13:33:22 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (54.03%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void merge(int[] a, int m, int[] b, int n) {
+     int k = m - 1;
+     int j = n - 1;
+     int i = 0;
+     for( i = m+n-1 ; i >= 0; --i){
+         
+         if(k < 0 || j < 0) break;
+         if(a[k] > b[j]){
+             a[i] = a[k];
+             --k;
+         } 
+         else {
+             a[i] = b[j];
+             --j;
+         }
+     }
+         while (j >= 0) { //only need to combine with remaining nums2, if any
+        a[i--] = b[j--]; }
+        
+    }
+}
