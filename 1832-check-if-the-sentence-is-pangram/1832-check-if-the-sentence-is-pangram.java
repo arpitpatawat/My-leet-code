e348c64f06098e29990ef96255b43813ff178335
arpitpatawat <75129076+arpitpatawat@users.noreply.github.com>
2022-05-28 00:29:59 +0530

    Time: 4 ms (27.10%), Space: 42.4 MB (29.68%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean checkIfPangram(String a) {
+        int count = 0;
+        for(int i = 0 ; i < a.length(); ++i){
+            char num = a.charAt(i);
+            if(a.indexOf(num) != i) count++;
+        }
+        return (a.length() - count ==26)? true:false;
+    }
+}
