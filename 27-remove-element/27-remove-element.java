4b603688c43008670fd05ce0c58b91390819c625
arpitpatawat <75129076+arpitpatawat@users.noreply.github.com>
2022-05-28 19:24:42 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (85.66%) - LeetHub

--- 27-remove-element/27-remove-element.java
+++ 27-remove-element/27-remove-element.java
@@ -3,9 +3,7 @@ class Solution {
        int res = 0;
         for(int i = 0 ; i < a.length ; ++i){
             if(a[i] != val){
-                // int temp = a[res];
                 a[res] = a[i];
-                // a[i] = temp;
                 ++res;
             }
             
