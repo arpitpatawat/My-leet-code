cca183845eac145ccd6e12fc0185bd0889e62eac
arpitpatawat <75129076+arpitpatawat@users.noreply.github.com>
2022-05-29 00:00:59 +0530

    Time: 10 ms (40.74%), Space: 43.8 MB (50.32%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -3,8 +3,8 @@ class Solution {
         if(s.length() != t.length()) return false;
         int arr[] = new int[26];
         for(int i = 0 ; i < s.length() ; ++i){
-            arr[s.charAt(i) - 'a']++;
-            arr[t.charAt(i) - 'a']--;
+            ++arr[s.charAt(i) - 'a'];
+            --arr[t.charAt(i) - 'a'];
         }
         for(int i = 0 ; i < 26 ; ++i){
             if(arr[i] != 0) return false;
