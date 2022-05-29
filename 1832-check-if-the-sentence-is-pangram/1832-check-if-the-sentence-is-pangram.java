f12eff1c72ba2661763079a0a3c4229f29bd6e14
arpitpatawat <75129076+arpitpatawat@users.noreply.github.com>
2022-05-29 16:17:34 +0530

    Time: 139 ms (5.54%), Space: 46 MB (5.33%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -3,6 +3,7 @@ class Solution {
         int count = 0;
         for(int i = 0 ; i < a.length(); ++i){
             char num = a.charAt(i);
+            System.out.println(a.indexOf(num));
             if(a.indexOf(num) != i) ++count;
         }
         return (a.length() - count ==26);
