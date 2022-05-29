240c46fb74bacf3d7a1e086cc6953a933e8480c7
arpitpatawat <75129076+arpitpatawat@users.noreply.github.com>
2022-05-29 19:44:36 +0530

    Time: 122 ms (5.00%), Space: 73.2 MB (5.16%) - LeetHub

---
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String reverse(String a, int low, int high){
+            char[] s= a.toCharArray();
+            while(low < high){
+                char temp = s[low];
+                s[low] = s[high];
+                s[high] = temp;
+                ++low; 
+                --high;
+            }
+        return new String(s);
+        // System.out.println(a);
+    }
+    public String reverseWords(String s) {
+        s = s.trim().replaceAll(" +"," "); // this will replace all the multiple white space in between and initial and after spaces of the string
+        int low = 0;
+       for(int i = 0 ; i < s.length() -1 ; ++i){
+           if(s.charAt(i) == ' '){
+              s = reverse(s,low , i -1);
+               low = i+1;
+               
+           }
+       } 
+        s=reverse(s,low , s.length() -1);
+        return reverse(s,0,s.length() -1);
+    }
+}
