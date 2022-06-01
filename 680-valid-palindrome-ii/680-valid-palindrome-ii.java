370a20c080c20d05b9f5c7127a5925f94b594e25
Arpit Patawat <75129076+arpitpatawat@users.noreply.github.com>
2022-06-01 09:39:30 +0530

    Time: 8 ms (83.32%), Space: 42.7 MB (95.07%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean check(String s, int low, int high){
+        while(low<high){
+            if(s.charAt(low) != s.charAt(high)){
+                return false;
+            }
+            --high;
+            ++low;
+        }
+        return true;
+    }
+    public boolean validPalindrome(String s) {
+        int low = 0;
+        int high = s.length() - 1;
+        while(low < high){
+            if(s.charAt(low) != s.charAt(high)){
+                return (check(s,low+1,high) || check(s,low,high-1));
+            }
+            --high;
+            ++low;
+        }
+        return true;
+    }
+}
