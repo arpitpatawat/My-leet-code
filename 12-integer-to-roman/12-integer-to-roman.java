33a0380d1958dd3f909aede58a9a8d3c1f244937
Arpit Patawat <75129076+arpitpatawat@users.noreply.github.com>
2022-06-01 10:32:11 +0530

    Time: 11 ms (40.94%), Space: 46.8 MB (21.08%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.java
+++ 12-integer-to-roman/12-integer-to-roman.java
@@ -3,8 +3,8 @@ class Solution {
         int value[] = {1000, 900, 500 , 400, 100 , 90 , 50 ,40 ,10 , 9,5,4 ,1};
         String symbol[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         String ans = "";
-        for(int i = 0 ; i < value.length ; ++i){
-            if(num==0) break;
+        for(int i = 0 ; i < value.length && num!= 0; ++i){
+            // if(num==0) break;
             while(num >= value[i]){
                 num -= value[i];
                 ans+= symbol[i];
