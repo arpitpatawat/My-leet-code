d5529dfc0f5114342db1da6b41665f62611e14b4
Arpit Patawat <75129076+arpitpatawat@users.noreply.github.com>
2022-06-01 10:34:55 +0530

    Time: 8 ms (63.34%), Space: 45.6 MB (48.19%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.java
+++ 12-integer-to-roman/12-integer-to-roman.java
@@ -1,15 +1,22 @@
 class Solution {
-    public String intToRoman(int num) {
+    public String intToRoman(int N) {
         int value[] = {1000, 900, 500 , 400, 100 , 90 , 50 ,40 ,10 , 9,5,4 ,1};
         String symbol[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-        String ans = "";
-        for(int i = 0 ; i < value.length && num!= 0; ++i){
-            // if(num==0) break;
-            while(num >= value[i]){
-                num -= value[i];
-                ans+= symbol[i];
+        // String ans = "";
+        // for(int i = 0 ; i < value.length && num!= 0; ++i){
+        //     // if(num==0) break;
+        //     while(num >= value[i]){
+        //         num -= value[i];
+        //         ans+= symbol[i];
+        //     }
+        // }
+         StringBuilder ans = new StringBuilder();
+        for (int i = 0; N > 0; i++)
+            while (N >= value[i]) {
+                ans.append(symbol[i]);
+                N -= value[i];
             }
-        }
-        return ans;
+        return ans.toString();
+        // return ans;
     }
 }
