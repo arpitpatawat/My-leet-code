8ef650a0933f671c5a3881115e561ab685522002
Arpit Patawat <75129076+arpitpatawat@users.noreply.github.com>
2022-06-03 00:42:05 +0530

    Time: 11 ms (68.01%), Space: 56.5 MB (33.11%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.java
@@ -0,0 +1,14 @@
+class Solution {
+     public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (int i = 0; i < strs.length ; ++i) {
+            char[] ca = new char[26];
+            for (char c : strs[i].toCharArray()) ca[c - 'a']++; // a good method to sort 
+            String keyStr = String.valueOf(ca);
+            if (!map.containsKey(keyStr)) map.put(keyStr, new ArrayList<>());
+            map.get(keyStr).add(strs[i]);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
