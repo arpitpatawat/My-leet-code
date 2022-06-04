2f7b16b4fb8c2397833080ff4436398ad12cb89a
Arpit Patawat <75129076+arpitpatawat@users.noreply.github.com>
2022-06-04 10:15:42 +0530

    Time: 2 ms (97.34%), Space: 42.4 MB (89.49%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -2,7 +2,7 @@ class Solution {
     List<List<String>> result = new ArrayList<>();
     public List<List<String>> solveNQueens(int n) {
         boolean[] visited = new boolean[n];
-        //2*n-1个斜对角线
+    
         boolean[] dia1 = new boolean[2*n-1];
         boolean[] dia2 = new boolean[2*n-1];
         
@@ -18,11 +18,11 @@ class Solution {
         }
         
         for(int i=0;i<n;i++){
-            //这一行、正对角线、反对角线都不能再放了，如果发现是true，停止本次循环
+            
             if(visited[i] || dia1[rowIndex+i] || dia2[rowIndex-i+n-1])
                 continue;
             
-            //init一个长度为n的一维数组，里面初始化为'.'
+           
             char[] charArray = new char[n];
             Arrays.fill(charArray,'.');
             
@@ -35,7 +35,7 @@ class Solution {
 
             fun(n,list,visited,dia1,dia2,rowIndex+1);
 
-            //reset 不影响回溯的下个目标
+            
             list.remove(list.size()-1);
             charArray[i] = '.';
             visited[i] = false;
