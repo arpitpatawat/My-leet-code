1cfcce64b014548dadc927a1dc160ff134659030
Arpit Patawat <75129076+arpitpatawat@users.noreply.github.com>
2022-06-11 08:30:45 +0530

    Time: 6 ms (83.80%), Space: 96 MB (54.63%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -1,35 +1,16 @@
 class Solution {
-    public int minOperations(int[] nums, int x) {
-        //
-        if(nums.length == 1 && nums[0] != x){
-             return -1;
-        }
-        
-        int arrsum = 0;
-        for(int i = 0 ; i < nums.length ; ++i){
-            arrsum += nums[i];
-        }
-        
-        // pre-processing
-        if(arrsum < x) return -1;
-        else if(arrsum == x) return nums.length;
-        int left = 0;
-        int right = 0;
-        int currsum = 0;
-        int maxsum = Integer.MAX_VALUE;
-        //nums[right] - nums[left] != arrsum - x
-        while(right<nums.length){
-            currsum += nums[right] ;
-            while(currsum > arrsum - x && left < right){
-                currsum -= nums[left];
-                ++left;
-            }
-            if(currsum == arrsum - x){
-                maxsum = Math.min(nums.length - right + left - 1, maxsum);
-            }
-            
-                ++right;
-        }
-        return  (maxsum ==Integer.MAX_VALUE) ? -1: maxsum;
-    }
+   public int minOperations(int[] nums, int x) {
+	int sum = 0;
+	for (int num: nums) sum += num;
+
+	int maxLength = -1, currSum = 0;
+	for (int l=0, r=0; r<nums.length; r++) {
+		currSum += nums[r];
+		while (l <= r && currSum > sum - x) currSum -= nums[l++];
+		if (currSum == sum - x) maxLength = Math.max(maxLength, r-l+1);
+	}
+
+	return maxLength == -1 ? -1 : nums.length - maxLength;
+}
+ 
 }
