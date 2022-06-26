c0a793f6e78f24ee9962eeeda91aaa9d8a387ca1
Arpit Patawat <75129076+arpitpatawat@users.noreply.github.com>
2022-06-26 10:15:45 +0530

    Time: 4 ms (30.67%), Space: 65.1 MB (63.31%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,16 +1,18 @@
 class Solution {
     public int maxScore(int[] a, int k) {
-      int t = a.length - k;
-        int arrsum = 0;
-    for(int i = 0 ; i < t ; ++i){
-        arrsum += a[i];
-    }
+        int t = a.length - k;  // min sum sliding window
+        int arrsum = 0; // total array sum
+        for(int i = 0 ; i < t ; ++i){
+            arrsum += a[i];
+        }
+        // in first pass , now i have array sum just before t. 
+        
         int currsum = arrsum;
         int minsum = currsum;
-    for(int i = t; i <= a.length -1; ++i){
-        currsum += a[i] - a[i-t];
-        minsum = Math.min(minsum,currsum);
-        arrsum += a[i];
+        for(int i = t; i <= a.length -1; ++i){
+            currsum += a[i] - a[i-t];
+            minsum = Math.min(minsum,currsum);
+            arrsum += a[i];
     }
         return arrsum - minsum;
     }
