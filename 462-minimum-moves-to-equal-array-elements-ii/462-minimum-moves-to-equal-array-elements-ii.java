4f5685125a54998729f475120513ebe5452dec9e
Arpit Patawat <75129076+arpitpatawat@users.noreply.github.com>
2022-06-30 09:56:57 +0530

    Time: 5 ms (66.60%), Space: 45.9 MB (36.27%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -3,8 +3,11 @@ class Solution {
         Arrays.sort(nums);
         int compare = nums[nums.length/2];
         int sum = 0;
-        for(int i = 0 ; i < nums.length ; ++i ){
-            sum += Math.abs(nums[i] - compare);
+        for(int i = 0 ; i < nums.length/2 ; ++i ){
+            sum += compare - nums[i];
+        }
+        for(int i = nums.length/2 ; i < nums.length; ++i ){
+            sum += nums[i] - compare;
         }
         return sum;
     }
