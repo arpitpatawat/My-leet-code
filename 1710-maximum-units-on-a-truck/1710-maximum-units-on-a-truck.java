78f864abe8bc6932012a27189c5bd3dcc4919c3e
Arpit Patawat <75129076+arpitpatawat@users.noreply.github.com>
2022-07-01 21:46:42 +0530

    Time: 12 ms (58.54%), Space: 50.6 MB (66.11%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
@@ -1,20 +1,13 @@
 class Solution {
     public int maximumUnits(int[][] nums, int truckSize) {
         Arrays.sort(nums , (a,b) -> b[1] - a[1]);
-        // for(int i = 0 ; i < nums.length; ++i){
-        //     for(int j = 0; j < 1 ; ++j){
-        //         System.out.println(nums[i][0] +","+ nums[i][1]);
-        //     } 
-        // }
         int count = 0;    
-        for(int i = 0 ; i < nums.length ; ++i){
-            if(truckSize ==0) break;
-            int temp = nums[i][0];
-            while(temp != 0 && truckSize != 0){
-                count += nums[i][1];
-                --temp;
-                --truckSize;
-            }
+        int i = 0;
+        while(truckSize > 0 && i< nums.length){
+            int minimum = Math.min(truckSize, nums[i][0]);
+            count += minimum * nums[i][1];
+            truckSize -= minimum;
+            ++i;    
         }
         return count ;
     }
