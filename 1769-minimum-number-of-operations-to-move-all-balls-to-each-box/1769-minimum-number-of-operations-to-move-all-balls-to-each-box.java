21039e9c553022652891b1e42239d4be343013a8
Arpit Patawat <75129076+arpitpatawat@users.noreply.github.com>
2022-07-27 12:19:16 +0530

    Time: 2 ms (99.73%), Space: 43 MB (86.48%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,19 +1,29 @@
 class Solution {
     public int[] minOperations(String boxes) {
-        HashSet<Integer> set = new HashSet<Integer>();
-        for(int i = 0 ; i < boxes.length() ; ++i){
+        int n = boxes.length();
+     int ans[] = new int[n];
+     // first pass to find all the indexes to reach 0's and total 1 ;
+     int count = 0;
+     int index = 0;
+        for(int i = 0 ; i < n ; ++i ){
             if(boxes.charAt(i) == '1'){
-                set.add(i);
+                ++count;
+                index += i;
             }
         }
-        int ans[] = new int[boxes.length()];
-        for(int i = 0 ; i <boxes.length() ; ++i ){
-            int sum = 0;
-            Iterator<Integer> it = set.iterator();
-            while(it.hasNext()){
-                    sum += Math.abs(it.next() - i);                
-            }
-            ans[i] = sum;
+        ans[0] = index;
+        
+        int fcount = 0;
+        if(boxes.charAt(0) == '1'){
+            count--;
+            fcount++;
+        }
+        for(int i = 1 ; i < n ; ++i){
+            ans[i] = ans[i-1] - count + fcount;
+            if(boxes.charAt(i) == '1'){
+            count--;
+            fcount++;
+        }
         }
         return ans;
     }
