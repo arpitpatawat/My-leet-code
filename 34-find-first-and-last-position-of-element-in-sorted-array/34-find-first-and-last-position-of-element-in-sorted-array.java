52ffdcf9e54c9ee5c5bb761b7dc4bcb0e5cbc8aa
Arpit Patawat <75129076+arpitpatawat@users.noreply.github.com>
2022-07-30 09:59:34 +0530

    Time: 0 ms (100.00%), Space: 47.4 MB (40.79%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,15 +1,46 @@
 class Solution {
-    public int[] searchRange(int[] a, int target) {
-        int start = -1, end = -1;
-        for(int i = 0 ; i < a.length ; ++i){
-            if(a[i] == target && start == -1){
-                start = i;
-            }
-            if(a[i] == target && start != -1){
-                end =i;
-            }
+    public int binarySearch1(int nums[] , int target){
+        int idx = -1;
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target) idx = mid;
+            if(nums[mid] < target){
+                low = mid + 1;
                 
+            }
+            else{
+                high = mid - 1;
+            }
         }
-        return new int[]{start,end};
+        return idx;
+        
+    }
+    public int binarySearch2(int nums[] , int target){
+        int idx = -1;
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target) idx = mid;
+            if(nums[mid] > target){
+                high = mid - 1;
+                
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return idx;
+        
+    }
+    public int[] searchRange(int[] nums, int target) {
+        // if(nums.length == 0) return new int[]{-1 , -1};
+        // 1st ka low and 2nd ka high output dega 
+        // int low = -1
+        int low = binarySearch1(nums, target);
+        int high = binarySearch2(nums , target );
+        return new int []{low ,high};
     }
 }
