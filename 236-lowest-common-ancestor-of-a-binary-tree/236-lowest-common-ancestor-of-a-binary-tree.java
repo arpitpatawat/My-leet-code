9bf5633d234a4bfcdab513f74c689f3aea4902b9
Arpit Patawat <75129076+arpitpatawat@users.noreply.github.com>
2022-07-30 23:22:44 +0530

    Time: 13 ms (28.67%), Space: 46.9 MB (79.11%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,47 +8,24 @@
  * }
  */
 class Solution {
-    public boolean helper(TreeNode root , int p , Set<Integer> h1){
-        if(root == null) return false;
-        
-        if(root.val == p){
-            h1.add(p);
-            return true;
-        }
-        
-        if(helper(root.left , p , h1) || helper(root.right , p , h1)){
-            h1.add(root.val);
-            return true;
-        }
-        
-        return false;
-        
-    }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        Set<Integer> h1 = new LinkedHashSet<>();
-        Set<Integer> h2 = new LinkedHashSet<>();
-      // change void to boolean  
-        if(helper( root ,  p.val ,  h1) && helper(root , q.val , h2)){
-        int min = Integer.MAX_VALUE;
-            if(h1.size() <= h2.size()){
-                for(int i : h1){
-                    if(h2.contains(i)){
-                        min = Math.min(min , i);
-                        return new TreeNode(min);
-                    }
-                }
-            }
-            else {
-                for(int i : h2){
-                    if(h1.contains(i)){
-                        min = Math.min(min , i);
-                        return new TreeNode(min);
-                    }
-                }
-            }
+        if(root == null) return null;
+        
+        if(root.val == p.val || root.val == q.val){
+            return root;
         }
-        return root;
         
+        TreeNode l = lowestCommonAncestor(root.left , p , q);
+        TreeNode r = lowestCommonAncestor(root.right, p , q);
         
+        if(l!= null && r!= null){
+            return root;
+        }
+        if(l!= null){
+            return l;
+        }
+        else{
+            return r;
+        }
     }
 }
