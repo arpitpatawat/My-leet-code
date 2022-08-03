c256e8813ec88d82559b159e02f1ce2e571e0400
Arpit Patawat <75129076+arpitpatawat@users.noreply.github.com>
2022-08-03 10:00:46 +0530

    Time: 2 ms (59.38%), Space: 49.6 MB (12.52%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,72 +1,23 @@
-// class Solution {
-//     public int trap(int[] height) {
-//         int n = height.length;
-//         int x[] = new int[n];
-//         int y[] = new int[n];
-//         int maxx = height[0], maxy = height[n-1];
-//         x[0] = maxx;
-//         y[n-1] = maxy;
-//         int j = 0,min=0,sum=0;
-//         for(int i = 1 ; i < n ; i++){
-//             j = n-1-i;
-//             maxx = Math.max(maxx, height[i]);
-//             maxy = Math.max(maxy,height[j]);
-//             x[i] = maxx;
-//             y[j] = maxy;      
-//         }
-//         for(int i = 0 ; i < n ; i++){
-//             min = Math.min(x[i], y[i]);
-//             sum += min - height[i];
-//         }
-//         return sum;
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class Solution {
     public int trap(int[] height) {
-        if (height == null || height.length == 0) {
-            return 0;
+        int n = height.length;
+        int left[] = new int[n];
+        int right[] = new int[n];
+        left[0] = height[0];
+        for(int i = 1 ; i < n; ++i){
+            left[i] = Math.max(height[i], left[i-1]);
+            
         }
-        int left = 0; int right = height.length - 1; 
-        int maxLeft = 0; int maxRight = 0;
+        right[n - 1] = height[n-1];
+        for(int i = n-2 ; i >= 0 ; --i){
+            right[i] = Math.max(right[i+1] , height[i]);
+        }
+        // preprocessing done
+        int sum = 0;
+        for(int i = 0 ; i < n; ++i){
+            sum += Math.min(left[i] , right[i]) - height[i];
+        }
         
-        int totalWater = 0;
-        while (left < right) {
-            if (height[left] < height[right]) {
-                if (height[left] >= maxLeft) { 
-                    maxLeft = height[left]; 
-                } 
-                else { 
-                    totalWater += maxLeft - height[left]; 
-                }
-                left++;
-                } 
-            else {
-                if (height[right] >= maxRight) { 
-                    
-                    maxRight = height[right]; 
-                } 
-                else {
-                    totalWater += maxRight - height[right]; 
-                }
-                right--;
-                }
-                 }
-        return totalWater;
+        return sum;
     }
 }
