d54a42f6537a91a4e6f34449cdd2144f2c5c3e0a
Arpit Patawat <75129076+arpitpatawat@users.noreply.github.com>
2022-08-04 19:42:07 +0530

    Time: 4 ms (86.83%), Space: 61.7 MB (80.01%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.java
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.java
@@ -1,35 +1,24 @@
-// class Solution {
-//     public int normalsum(int arr[],int n){
-//         int res = arr[0], max = arr[0];
-//         for(int i = 1 ; i < n ; i++){
-//             max = Math.max(arr[i], arr[i] + max);
-//             res = Math.max(res, max);
-//         }
-//         return res;
-//     }
-//     public int maxSubarraySumCircular(int[] nums) {
-//          int kadane = normalsum(nums,nums.length);
-//         if(kadane < 0) return kadane;
-//         int arrsum = 0;
-//         for(int i = 0 ; i < nums.length ; i++){
-//             arrsum += nums[i];
-//             nums[i] = -nums[i];
-//         }
-//         int cirsum =arrsum + normalsum(nums, nums.length);
-//         System.out.println(kadane +"," + cirsum);
-//         return Math.max(kadane,cirsum);
-//     }
-// }
-class Solution{
-  public int maxSubarraySumCircular(int[] A) {
-        int total = 0, maxSum = A[0], curMax = 0, minSum = A[0], curMin = 0;
-        for (int a : A) {
-            curMax = Math.max(curMax + a, a);
-            maxSum = Math.max(maxSum, curMax);
-            curMin = Math.min(curMin + a, a);
-            minSum = Math.min(minSum, curMin);
-            total += a;
+class Solution {
+    public int kadane(int nums[]){
+        int sum = nums[0];
+        int curr = nums[0];
+        for(int i = 1 ; i < nums.length ; ++i){
+            curr = Math.max(curr + nums[i] , nums[i]);
+            sum = Math.max(curr , sum);
         }
-        return maxSum > 0 ? Math.max(maxSum, total - minSum) : maxSum;
+        return sum;
+    }
+    public int maxSubarraySumCircular(int[] nums) {
+        
+        int linear = kadane(nums);
+        int total = 0;
+        for(int i = 0 ; i < nums.length ; ++i){
+            total += nums[i];
+            nums[i] = -nums[i];
+        }
+        int negativeLinear = kadane(nums);
+        int circular = total + negativeLinear;
+        if(circular == 0) return linear;
+        return (linear > circular) ? linear : circular;
     }
 }
