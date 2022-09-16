5c92737ea868417f3fa3be17ce034159d8694232
Arpit Patawat <75129076+arpitpatawat@users.noreply.github.com>
2022-09-16 18:12:57 +0530

    Time: 107 ms (62.76%), Space: 61.8 MB (87.82%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -1,14 +1,14 @@
 class Solution {
-    int N, M;
+    
     public int maximumScore(int[] nums, int[] multipliers) {
-        N = nums.length;
-        M = multipliers.length;
+       int N = nums.length;
+       int M = multipliers.length;
 	    return helper(nums, multipliers, 0, 0, new Integer[M][M]);
     }
 
     private int helper(int[] nums, int[] multipliers, int left, int index, Integer[][] dp) {
-	    int right = N - 1 - (index - left);
-	    if (index == M) return 0;
+	    int right = nums.length - 1 - (index - left);
+	    if (index == multipliers.length) return 0;
 
 	    if (dp[left][index] != null) return dp[left][index];
 
