84beedd403ea81773c469e642f3d2a4049a8e39c
Arpit Patawat <75129076+arpitpatawat@users.noreply.github.com>
2022-10-01 15:24:28 +0530

    Time: 1 ms (99.81%), Space: 45.4 MB (90.31%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -15,8 +15,8 @@ public class Solution {
         ListNode b = headB;
         while(a != b){
             a = (a == null) ? headB : a.next;
-            b = (b == null) ? headA : b.next; 
+            b = (b == null) ? headA : b.next;
         }
-        return a;
+        return (a == null) ? null : a; 
     }
 }
