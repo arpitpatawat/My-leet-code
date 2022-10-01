46a19e5a2becb9246b46a3108e57e13ac77a9195
Arpit Patawat <75129076+arpitpatawat@users.noreply.github.com>
2022-10-01 16:24:36 +0530

    Time: 1 ms (81.87%), Space: 42.5 MB (79.44%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -1,24 +1,66 @@
-public class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode head = new ListNode(0);
-        ListNode handler = head;
-        while(l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                handler.next = l1;
-                l1 = l1.next;
-            } else {
-                handler.next = l2;
-                l2 = l2.next;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode h1, ListNode h2) {
+        // case 1: one of the list is null
+          { 
+              if(h1 == null){
+                return h2;
             }
-            handler = handler.next;
+            if(h2 == null){
+                return h1;
+            }
+          }
+        
+        // case 2 : deciding head 
+        
+            ListNode head;
+            ListNode tail = null;
+            if(h1.val <= h2.val){
+                head = h1;
+                tail = h1;
+                h1 = h1.next;
+            }
+            else{
+                head = h2;
+                tail = h2;
+                h2 = h2.next;
+            }
+        
+        // case 3: sorting and merging
+        {
+            while(h1 != null && h2 != null){
+                if(h1.val <= h2.val){
+                    tail.next = h1;
+                    
+                    h1 = h1.next;
+                }
+                else{
+                    tail.next =h2;
+                    h2 = h2.next;
+                }
+                tail = tail.next;
+            }
+        }
+        // case 4: if one of the list is finished 
+        {
+            if(h1 == null){
+                tail.next = h2;
+            }
+            else if(h2 == null ){
+                tail.next = h1;
+            }
+            
         }
         
-        if (l1 != null) {
-            handler.next = l1;
-        } else if (l2 != null) {
-            handler.next = l2;
-        }
-        
-        return head.next;
+        return head;
     }
 }
