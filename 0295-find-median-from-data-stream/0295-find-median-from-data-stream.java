e886dfc4ed77be35828db2dfe8bcc31d8dacfb56
Arpit Patawat <75129076+arpitpatawat@users.noreply.github.com>
2022-11-12 09:16:34 +0530

    Time: 136 ms (90.67%), Space: 60.9 MB (95.42%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,56 +1,37 @@
 class MedianFinder {
-        ArrayList<Integer> arr;
+
        
     public MedianFinder() {
-        arr = new ArrayList<>();
+        
        
     }
     
-    public void addNum(int num) {
-        int index = arr.size() - 1;
-        if(index == -1 || arr.get(index) <= num){
-            arr.add(num);
-            ++index;
-            return ;
-        }
-        int low = 0;
-        int high = index;
-        while(low <= high){
-            int mid = low + (high - low)/2;
-            if(arr.get(mid) == num){
-                arr.add(mid,num);
-                ++index;
-                return ;
-            }
-            else if(arr.get(mid) < num){
-                low = mid + 1;
-            }
-            else{
-                high = mid - 1;
-            }
-        }
-        arr.add(low, num);
-        ++index;
-        return ;
-    }
-    
-    public double findMedian() {
-        // System.out.println(arr);
-        // if index -> odd then even element and vice versa
-        int index = arr.size() - 1;
-        if(index %2 == 0){
-            int element = index / 2;
-            double d = arr.get(element);
-            return d;
-        }
-        else{
-            int element = index / 2;
-            double d = arr.get(element) + arr.get(element + 1);
-            return d/2;
-        }
+    private PriorityQueue<Integer> small = new PriorityQueue<>(Collections.reverseOrder());
+private PriorityQueue<Integer> large = new PriorityQueue<>();
+private boolean even = true;
 
-    }
+public double findMedian() {
+    if (even)
+        return (small.peek() + large.peek()) / 2.0;
+    else
+        return small.peek();
 }
+
+public void addNum(int num) {
+    if (even) {
+        large.offer(num);
+        small.offer(large.poll());
+    } else {
+        small.offer(num);
+        large.offer(small.poll());
+    }
+    // System.out.println(small);
+    // System.out.println(large);
+    even = !even;
+}    
+
+}
+
 
 /**
  * Your MedianFinder object will be instantiated and called as such:
