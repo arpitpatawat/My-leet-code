6e001c17b344e0992273ff538aa843400dfd7796
Arpit Patawat <75129076+arpitpatawat@users.noreply.github.com>
2022-11-12 15:43:11 +0530

    Time: 276 ms (29.79%), Space: 127.5 MB (6.95%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,38 +1,32 @@
 class MedianFinder {
 
-       
-    public MedianFinder() {
-        
-       
-    }
-    
-    private PriorityQueue<Integer> small = new PriorityQueue<>(Collections.reverseOrder());
+    public MedianFinder() {}
+
+private PriorityQueue<Integer> small = new PriorityQueue<>(Collections.reverseOrder());
 private PriorityQueue<Integer> large = new PriorityQueue<>();
 private boolean even = true;
 
 public double findMedian() {
-    if (even)
-        return (small.peek() + large.peek()) / 2.0;
-    else
-        return small.peek();
+        if(even){
+            return ( small.peek() + large.peek() ) / 2.0;
+        }
+    else{
+        return large.peek();
+    }
 }
 
 public void addNum(int num) {
     if (even) {
-        large.offer(num);
-        small.offer(large.poll());
-    } else {
         small.offer(num);
         large.offer(small.poll());
+    } else {
+        large.offer(num);
+        small.offer(large.poll());
+
     }
-    // System.out.println(small);
-    // System.out.println(large);
     even = !even;
-}    
-
 }
-
-
+}
 /**
  * Your MedianFinder object will be instantiated and called as such:
  * MedianFinder obj = new MedianFinder();
